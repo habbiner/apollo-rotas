bc7fb92f8bfc5527f4e2d6acc415d49a3a40feb8
Habbiner Soares de Andrade <habbiner@jasaudeanimal.com.br>
2023-10-24 21:56:44 -0300

    Commit paginas: Voo, Pacotes, Home e os 2 CSS

---
+++ js/app.js
@@ -0,0 +1,46 @@
+// Adicione este código JavaScript para controlar o carrossel
+document.addEventListener('DOMContentLoaded', function () {
+    const slideControllers = document.querySelectorAll('.slide-controller');
+    const slides = document.querySelectorAll('.slide');
+    let currentIndex = 0;
+
+    slideControllers.forEach(function (controller) {
+        controller.addEventListener('click', function () {
+            if (this.dataset.action === 'prev') {
+                slides[currentIndex].classList.remove('visible');
+                currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+                slides[currentIndex].classList.add('visible');
+            } else if (this.dataset.action === 'next') {
+                slides[currentIndex].classList.remove('visible');
+                currentIndex = (currentIndex + 1) % slides.length;
+                slides[currentIndex].classList.add('visible');
+            }
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const rangeInput1 = document.getElementById('range1');
+    const clock1 = document.getElementById('clock1');
+
+    rangeInput1.addEventListener('input', function () {
+        updateClock(rangeInput1, clock1);
+    });
+
+    const rangeInput2 = document.getElementById('range2');
+    const clock2 = document.getElementById('clock2');
+
+    rangeInput2.addEventListener('input', function () {
+        updateClock(rangeInput2, clock2);
+    });
+
+    function updateClock(input, clock) {
+        const rangeValue = parseInt(input.value);
+        const hours = Math.floor(rangeValue / 100 * 24);
+        const minutes = Math.floor((rangeValue % 100) / 100 * 60);
+        const seconds = Math.floor(((rangeValue % 100) % 100) / 100 * 60);
+
+        const formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
+        clock.textContent = formattedTime;
+    }
+});
